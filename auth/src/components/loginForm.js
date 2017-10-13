b22e78e2a1604be94e90e3806df5e285713981ee
Ankit Arora <ankit992@live.in>
2017-10-14 01:54:53 +0530

    creating spinner for showing wait

--- auth/src/components/loginForm.js
+++ auth/src/components/loginForm.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection, Input } from './common'
+import { Text } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
     state = { 
         email: '',
-        password: '' 
+        password: '',
+        error: ''
     };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '' });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed' });
+                    });
+            });
+    }
 
     render() {
         return (
@@ -29,12 +46,24 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
-                    <Button>Log in</Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
                 </CardSection>
             </Card>
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize:20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
 
 export default LoginForm;
