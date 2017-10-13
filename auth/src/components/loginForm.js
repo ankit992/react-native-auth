4afbc373ee8fb1a603eb5e13c85c1650942d48e7
Ankit Arora <ankit992@live.in>
2017-10-14 02:03:56 +0530

    show spinner on sending request

--- auth/src/components/loginForm.js
+++ auth/src/components/loginForm.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 import firebase from 'firebase';
 
 class LoginForm extends Component {
     state = { 
         email: '',
         password: '',
-        error: ''
+        error: '',
+        loading: false
     };
 
     onButtonPress() {
         const { email, password } = this.state;
 
-        this.setState({ error: '' });
+        this.setState({ error: '', loading: true });
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .catch(() => {
@@ -22,6 +23,16 @@ class LoginForm extends Component {
                         this.setState({ error: 'Authentication Failed' });
                     });
             });
+    }
+
+    renderButton() {
+        if(this.state.loading){
+            return <Spinner size='small' />
+        }
+        
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
+        );
     }
 
     render() {
@@ -51,7 +62,7 @@ class LoginForm extends Component {
                 </Text>
 
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
